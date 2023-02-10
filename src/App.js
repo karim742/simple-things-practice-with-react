608c81389813427aecd3c0ddd11f7d0f4191144b
karim742 <121242355+karim742@users.noreply.github.com>
2023-02-10 09:36:27 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+  console.log('hello');
   return (
     <div>
-      <h3>React practice with StackBlitz!</h3>
+      <h3 style={{ color: 'red' }}>React practice with StackBlitz!</h3>
+      <h4>Checkbox with react</h4>
+
+      <div className="container">
+        <label htmlFor="HSC">HSC</label>
+        <input type="checkbox" id="HSC" value="HSC" />
+        <br />
+        <label htmlFor="SSC">SSC</label>
+        <input type="checkbox" id="SSC" value="SSC" />
+        <br />
+
+        <label htmlFor="JSC">JSC</label>
+        <input type="checkbox" id="JSC" value="JSC" />
+      </div>
     </div>
   );
 }
