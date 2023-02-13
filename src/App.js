47f4a0dd2aaf04fab9d5d4d7f64580c63ef7037a
karim742 <121242355+karim742@users.noreply.github.com>
2023-02-13 16:28:28 +0600

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
 import React from 'react';
 import './style.css';
 
+import Form from './Form';
+
 export default class App extends React.Component {
-  state = {
-    education: '',
-  };
-
-  submit = (e) => {
-    this.setState({
-      [e.target.name]: e.target.checked,
-    });
-  };
-
   render() {
     return (
       <>
-        <div>
-          <h3 style={{ color: 'red' }}>React practice with StackBlitz!</h3>
-          <h4>Checkbox with react</h4>
-
-          <div className="container">
-            <label htmlFor="HSC">HSC</label>
-            <input
-              type="checkbox"
-              id="HSC"
-              value="HSC"
-              name="HSC"
-              onChange={this.submit}
-            />
-            <br />
-            <label htmlFor="SSC">SSC</label>
-            <input
-              type="checkbox"
-              id="SSC"
-              value="SSC"
-              name="SSC"
-              onChange={this.submit}
-            />
-            <br />
-
-            <label htmlFor="JSC">JSC</label>
-            <input
-              type="checkbox"
-              id="JSC"
-              value="JSC"
-              name="JSC"
-              onChange={this.submit}
-            />
-          </div>
-          <br />
-          <button
-            type="button"
-            onClick={() => {
-              console.log(this.state);
-            }}
-          >
-            Add Checkbox
-          </button>
+        <div className="signup">
+          <h2>SIGN UP</h2>
         </div>
+        <Form />
       </>
     );
   }
---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Input from './Inputbox';
+
+export default function Form() {
+  return (
+    <>
+      <form action="#">
+        <Input />
+      </form>
+    </>
+  );
+}
