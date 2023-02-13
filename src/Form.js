d3dc7940cc3b73dff9624a61be3b98f124a3499c
karim742 <121242355+karim742@users.noreply.github.com>
2023-02-13 16:35:03 +0600

    1 file modified

--- src/Form.js
+++ src/Form.js
@@ -5,7 +5,19 @@ export default function Form() {
   return (
     <>
       <form action="#">
-        <Input label="Name: " type="text" placeholder="name" />
+        <Input label="Name" type="text" placeholder="name" /> <br /> <br />
+        <Input label="Email" type="email" placeholder="email" /> <br /> <br />
+        <Input
+          label="Password"
+          type="password"
+          placeholder="password"
+        /> <br /> <br />
+        <Input
+          label="Confirm-Password"
+          type="password"
+          placeholder="confirm-password"
+        />{' '}
+        <br /> <br />
       </form>
     </>
   );
