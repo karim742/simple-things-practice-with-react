bfe358238ed94a35062c66f14639a1e9b799f10f
karim742 <121242355+karim742@users.noreply.github.com>
2023-02-13 18:01:18 +0600

    1 file modified

--- src/Form.js
+++ src/Form.js
@@ -3,29 +3,41 @@ import Input from './Inputbox';
 import Radio from './Radio';
 import Checkbox from './Checkbox';
 
-export default function Form() {
-  return (
-    <>
-      <form action="#">
-        <Input label="Name: " type="text" placeholder="name" /> <br />
-        <Input label="Email: " type="email" placeholder="email" /> <br />
-        <Input label="Password: " type="password" placeholder="password" />{' '}
-        <br />
-        <Input
-          label="Confirm-Password: "
-          type="password"
-          placeholder="confirm-password"
-        />
-        <br />
-        <Radio name="Male" />
-        <Radio name="Female" />
-        <br />
-        <br />
-        <Checkbox text="I agree all tarms & condition" />
-        <br />
-        <br />
-        <Input type="submit" value="Submit" />
-      </form>
-    </>
-  );
+export default class Form extends React.Component {
+  render() {
+    return (
+      <>
+        <form action="#">
+          <Input label="Name: " type="text" placeholder="name" /> <br />
+          <Input label="Email: " type="email" placeholder="email" /> <br />
+          <Input
+            label="Password: "
+            type="password"
+            placeholder="password"
+          />{' '}
+          <br />
+          <Input
+            label="Confirm-Password: "
+            type="password"
+            placeholder="confirm-password"
+          />
+          <br />
+          <Radio name="Male" />
+          <Radio name="Female" />
+          <br />
+          <br />
+          <Checkbox text="I agree all tarms & condition" />
+          <br />
+          <br />
+          <button
+            type="button"
+            value="Submit"
+            onClick={() => console.log('hello')}
+          >
+            Submit
+          </button>
+        </form>
+      </>
+    );
+  }
 }
