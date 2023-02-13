c120894d5f152cdd53593fed91fa079c5b1a5efc
karim742 <121242355+karim742@users.noreply.github.com>
2023-02-13 16:39:52 +0600

    1 file modified, 1 added

--- src/Form.js
+++ src/Form.js
@@ -5,19 +5,15 @@ export default function Form() {
   return (
     <>
       <form action="#">
-        <Input label="Name" type="text" placeholder="name" /> <br /> <br />
-        <Input label="Email" type="email" placeholder="email" /> <br /> <br />
-        <Input
-          label="Password"
-          type="password"
-          placeholder="password"
-        /> <br /> <br />
+        <Input label="Name" type="text" placeholder="name" /> <br />
+        <Input label="Email" type="email" placeholder="email" /> <br />
+        <Input label="Password" type="password" placeholder="password" /> <br />
         <Input
           label="Confirm-Password"
           type="password"
           placeholder="confirm-password"
         />{' '}
-        <br /> <br />
+        <br />
       </form>
     </>
   );
---
+++ src/Radio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Radio() {
+  return (
+    <>
+      <label htmlFor={name}></label>
+      <input type={type} id={name} />
+    </>
+  );
+}
